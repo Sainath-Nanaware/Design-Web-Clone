fa6203964acb4e7f834c1f3333cf0b6dbbf51484
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-01-28 23:13:09 +0530

    create div

--- src/components/ClientAboutUs.jsx
+++ src/components/ClientAboutUs.jsx
@@ -8,6 +8,10 @@ function ClientAboutUs() {
             <p className='w-[30vw] text-center text-[#90A3B4]'>Event madness gathering innoies,& tech enthusiasts in Speced.
 do more informations.</p>
         </div>
+        {/* for image */}
+        <div>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae. Consequuntur repellat consequatur praesentium at laudantium, aut voluptatibus, esse quas, beatae perspiciatis laborum? Quae minus veniam earum facilis dignissimos sunt similique omnis nemo explicabo quam libero, sed labore sequi perferendis accusamus provident facere repellat a dolores! In mollitia aliquid molestiae!
+        </div>
       
     </div>
   )
