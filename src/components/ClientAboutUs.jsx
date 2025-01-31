d058cc5c732f80971d5c29a207ceabc4c461dd7f
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-01-31 23:26:49 +0530

    add text div

--- src/components/ClientAboutUs.jsx
+++ src/components/ClientAboutUs.jsx
@@ -11,6 +11,8 @@ do more informations.</p>
         {/* for image */}
         <div>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, molestiae. Consequuntur repellat consequatur praesentium at laudantium, aut voluptatibus, esse quas, beatae perspiciatis laborum? Quae minus veniam earum facilis dignissimos sunt similique omnis nemo explicabo quam libero, sed labore sequi perferendis accusamus provident facere repellat a dolores! In mollitia aliquid molestiae!
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate quis tempore veritatis modi quisquam. Obcaecati repellat natus, illo magnam consequuntur, tenetur id quisquam iure ducimus dolorum quibusdam laborum voluptatibus facilis!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis a cum expedita vero? Dolorum doloribus neque voluptatum fuga cumque laborum perspiciatis, aliquid laudantium delectus rem ipsam cupiditate, omnis adipisci optio facilis a quisquam? Accusamus nostrum provident explicabo placeat omnis architecto reprehenderit et voluptatibus, ullam, repellat amet at possimus nesciunt atque minus. Ab quos molestiae excepturi?
         </div>
       
     </div>
