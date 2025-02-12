a6c4ef67ff295ee7859c1b69caf09a3ac3cf8278
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-02-12 19:00:31 +0530

    add text div

--- src/components/ClientAboutUs.jsx
+++ src/components/ClientAboutUs.jsx
@@ -14,6 +14,8 @@ do more informations.</p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi est explicabo error ullam tempora, veniam neque dolore unde iusto cumque quidem libero rem! Odio excepturi natus nostrum reprehenderit consequatur eligendi nesciunt! Provident consequuntur nobis illum facere consectetur? Aspernatur obcaecati assumenda adipisci minima, id reiciendis aperiam doloremque in laudantium quos officia dolores eligendi. Culpa, eaque. Autem!
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore saepe id aliquid autem nihil commodi debitis perferendis tempore hic aspernatur. Voluptates temporibus laboriosam reprehenderit et sit earum commodi molestiae cumque iste labore. Sapiente eos omnis totam, natus, vitae itaque ducimus aspernatur dicta officiis explicabo magni aliquam possimus ad quisquam officia sit doloribus amet quod similique quidem consequatur. Odit maxime, rerum, quibusdam in omnis molestias illo minima facere neque commodi modi reprehenderit cupiditate optio reiciendis voluptatem beatae exercitationem atque at. Dolores ad excepturi exercitationem aliquid voluptate. Ipsa, nulla natus.
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ullam accusamus iusto voluptate deleniti maxime repudiandae officiis? Obcaecati officiis, qui possimus quibusdam consequuntur fuga ratione, impedit natus sequi veniam, harum molestias a. Odit ut quasi doloribus similique nam. Totam culpa ducimus architecto ut, adipisci dolorem?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis maiores ab nam laudantium ducimus! Nulla suscipit eligendi corporis eaque deserunt voluptas id, laborum officiis beatae repudiandae qui omnis dignissimos!
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt quam harum excepturi assumenda recusandae, placeat voluptatum consequatur autem officia provident nobis unde cumque minus dolores doloremque, optio, rerum laudantium dolorem?
         </div>
       
     </div>
@@ -21,3 +23,4 @@ do more informations.</p>
 }
 
 export default ClientAboutUs
+
